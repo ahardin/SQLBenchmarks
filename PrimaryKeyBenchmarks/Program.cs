dae7f08965dec603efcdd5e8d3784d0a19b82067
Alex Hardin <alexchardin@gmail.com>
2012-08-24 10:39:24 -0500

    added the files

---
+++ PrimaryKeyBenchmarks/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace PrimaryKeyBenchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Plain"].ConnectionString))
+            {
+                conn.Open();
+                using (System.IO.StreamReader reader = new System.IO.StreamReader("database.sql", true))
+                {
+                    string[] statements = reader.ReadToEnd().Split(new string[] { "GO" }, StringSplitOptions.RemoveEmptyEntries);
+                    var command = conn.CreateCommand();
+                    command.CommandType = System.Data.CommandType.Text;
+
+                    foreach (string statement in statements)
+                    {
+                        command.CommandText = statement;
+                        command.ExecuteNonQuery();
+                    }
+                    
+                    conn.Close();
+                }
+            }
+
+            Console.WriteLine("Starting integer test...");
+
+            int numberOfTrials = 10;
+            int numberOfInserts = 1000;
+            double[] integerRunTimes = new double[numberOfTrials];
+            double[] guidRunTimes = new double[numberOfTrials];
+            DateTime start;
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Plain"].ConnectionString))
+            {
+                conn.Open();
+                var command = conn.CreateCommand();
+                command.CommandType = System.Data.CommandType.Text;
+
+                for (int x = 0; x < numberOfTrials; x++)
+                {
+                    Console.WriteLine("Deleting {0}", x);
+
+                    command.CommandText = "TRUNCATE TABLE [Integer]";
+                    command.ExecuteNonQuery();
+
+                    StringBuilder b = new StringBuilder();
+
+                    Console.WriteLine("Building query {0}", x);
+                    for (int i = 0; i < numberOfInserts; i++)
+                    {
+                        b.AppendLine(String.Format("INSERT INTO [Integer] ([Value]) VALUES ({0})", i.ToString()));
+                    }
+                    
+                    Console.WriteLine("Executing query {0}", x);
+                    start = DateTime.Now;
+                    command.CommandText = b.ToString();
+                    command.ExecuteNonQuery();
+
+                    DateTime end = DateTime.Now;
+
+                    Console.WriteLine("Integer run {0} time: {1}ms", x, (end - start).TotalMilliseconds);
+                    integerRunTimes[x] = (end - start).TotalMilliseconds;
+                }
+
+                for (int x = 0; x < numberOfTrials; x++)
+                {
+                    Console.WriteLine("Deleting {0}", x);
+
+                    command.CommandText = "TRUNCATE TABLE [Guid]";
+                    command.ExecuteNonQuery();
+
+                    StringBuilder b = new StringBuilder();
+
+                    Console.WriteLine("Building query {0}", x);
+                    for (int i = 0; i < numberOfInserts; i++)
+                    {
+                        b.AppendLine(String.Format("INSERT INTO [Guid] ([Value]) VALUES ({0})", i.ToString()));
+                    }
+
+                    Console.WriteLine("Executing query {0}", x);
+                    start = DateTime.Now;
+                    command.CommandText = b.ToString();
+                    command.ExecuteNonQuery();
+
+                    DateTime end = DateTime.Now;
+
+                    Console.WriteLine("Guid run {0} time: {1}ms", x, (end - start).TotalMilliseconds);
+                    guidRunTimes[x] = (end - start).TotalMilliseconds;
+                }
+
+                conn.Close();
+            }
+
+            Console.WriteLine("Average Integer time: {0}ms", integerRunTimes.Where(i => i > 0).Average());
+            Console.WriteLine("Average Guid time: {0}ms", guidRunTimes.Where(i => i > 0).Average());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PrimaryKeyBenchmarks/database.sql
@@ -0,0 +1,66 @@
+﻿USE [master]
+GO
+/****** Object:  Database [PrimaryKeyBenchmarks]    Script Date: 08/23/2012 18:27:24 ******/
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'PrimaryKeyBenchmarks')
+BEGIN
+	CREATE DATABASE [PrimaryKeyBenchmarks] 
+END
+
+GO
+USE [PrimaryKeyBenchmarks]
+GO
+/****** Object:  Table [dbo].[Guid]    Script Date: 08/23/2012 18:27:26 ******/
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF_Guid_Key]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[Guid] DROP CONSTRAINT [DF_Guid_Key]
+END
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Guid]') AND type in (N'U'))
+DROP TABLE [dbo].[Guid]
+GO
+/****** Object:  Table [dbo].[Integer]    Script Date: 08/23/2012 18:27:26 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Integer]') AND type in (N'U'))
+DROP TABLE [dbo].[Integer]
+GO
+/****** Object:  Table [dbo].[Integer]    Script Date: 08/23/2012 18:27:26 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Integer]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[Integer](
+	[Key] [int] IDENTITY(1,1) NOT NULL,
+	[Value] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Integer] PRIMARY KEY CLUSTERED 
+(
+	[Key] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[Guid]    Script Date: 08/23/2012 18:27:26 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Guid]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[Guid](
+	[Key] [uniqueidentifier] NOT NULL CONSTRAINT [DF_Guid_Key]  DEFAULT (newid()),
+	[Value] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Guid] PRIMARY KEY CLUSTERED 
+(
+	[Key] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+SET ANSI_PADDING OFF
+GO
